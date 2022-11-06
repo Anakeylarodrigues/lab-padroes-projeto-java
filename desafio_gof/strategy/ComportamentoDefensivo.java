9b5c797f868088a7e014a44e838621257cacc1ed
Anakeylarodrigues <anakeylarodrigues1@gmail.com>
2022-11-06 11:02:09 -0300

    finish

---
+++ desafio_gof/strategy/ComportamentoDefensivo.java
@@ -0,0 +1,9 @@
+package desafio_gof.strategy;
+
+public class ComportamentoDefensivo implements Comportamento {
+
+    public void mover() {
+        System.out.println("Movendo-se defensivamente...");
+    }
+    
+}
